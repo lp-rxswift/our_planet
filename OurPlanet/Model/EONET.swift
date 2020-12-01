dd19fe1e2a45aaa7d1627a6e205659163d9ab110
Lucas <lucaspedrazoli@gmail.com>
2020-12-01 17:45:17 -0300

    add endpoint parameter for events in EONET service

--- OurPlanet/Model/EONET.swift
+++ OurPlanet/Model/EONET.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import Foundation
 import RxSwift
 import RxCocoa
@@ -94,9 +62,9 @@ class EONET {
       .sorted(by: EOEvent.compareDates)
   }
 
-  static func events(forLast days: Int = 360) -> Observable<[EOEvent]> {
-    let openEvents = events(forLast: days, closed: false)
-    let closedEvents = events(forLast: days, closed: true)
+  static func events(forLast days: Int = 360, category: EOCategory) -> Observable<[EOEvent]> {
+    let openEvents = events(forLast: days, closed: false, endpoint: category.endpoint)
+    let closedEvents = events(forLast: days, closed: true, endpoint: category.endpoint)
     return Observable.of(openEvents, closedEvents)
       .merge()
       .reduce([]) { running, new in
@@ -105,12 +73,13 @@ class EONET {
   }
 
   private static func events(forLast days: Int,
-                             closed: Bool) -> Observable<[EOEvent]> {
+                             closed: Bool,
+                             endpoint: String) -> Observable<[EOEvent]> {
     let query: [String: Any] = [
       "days": days,
       "status": (closed ? "closed": "open")
     ]
-    let request: Observable<[EOEvent]> = EONET.request(endpoint: eventsEndpoint,
+    let request: Observable<[EOEvent]> = EONET.request(endpoint: endpoint,
                                                        query: query,
                                                        contentIdentifier: "events")
     return request.catchErrorJustReturn([])
